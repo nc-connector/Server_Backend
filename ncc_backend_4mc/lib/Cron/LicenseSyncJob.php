8e173dd9a3a2a8f9d57f7a2ed7c527bfba6485a7
DeBaschdi <debaschdi@googlemail.com>
2026-04-01 05:07:49 +0200

    Fix LicenseSyncJob

--- ncc_backend_4mc/lib/Cron/LicenseSyncJob.php
+++ ncc_backend_4mc/lib/Cron/LicenseSyncJob.php
@@ -10,32 +10,62 @@ declare(strict_types=1);
 
 namespace OCA\NcConnector\Cron;
 
+use OCA\NcConnector\AppInfo\Application;
 use OCA\NcConnector\Service\LicenseService;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\BackgroundJob\TimedJob;
 use Psr\Log\LoggerInterface;
 
 class LicenseSyncJob extends TimedJob {
+	private ?LicenseService $license = null;
+	private ?LoggerInterface $logger = null;
+
 	public function __construct(
 		ITimeFactory $timeFactory,
-		private LicenseService $license,
-		private LoggerInterface $logger,
 	) {
 		parent::__construct($timeFactory);
 		$this->setInterval(24 * 60 * 60);
 	}
 
 	protected function run($argument): void {
-		if (!$this->license->canContactLicenseServer()) {
-			return;
+		try {
+			$license = $this->getLicenseService();
+			if (!$license->canContactLicenseServer()) {
+				return;
+			}
+
+			$this->getLogger()->debug('Running license sync (24h job)');
+			$license->syncNow();
+		} catch (\Throwable $exception) {
+			$logger = $this->logger;
+			if ($logger === null) {
+				try {
+					$logger = $this->getLogger();
+				} catch (\Throwable) {
+					throw $exception;
+				}
+			}
+
+			$logger->error('License sync background job failed', [
+				'exception' => $exception,
+			]);
+			throw $exception;
 		}
-
-		$this->logDebug('Running license sync (24h job)');
-
-		$this->license->syncNow();
 	}
 
-	private function logDebug(string $message): void {
-		$this->logger->debug($message);
+	private function getLicenseService(): LicenseService {
+		if ($this->license === null) {
+			$this->license = (new Application())->getContainer()->get(LicenseService::class);
+		}
+
+		return $this->license;
+	}
+
+	private function getLogger(): LoggerInterface {
+		if ($this->logger === null) {
+			$this->logger = (new Application())->getContainer()->get(LoggerInterface::class);
+		}
+
+		return $this->logger;
 	}
 }
